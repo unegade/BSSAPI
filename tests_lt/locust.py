925282e6af88674be46b70585aa02f4f8a8a9362
unegade <unegade@gmail.com>
2019-10-18 02:38:35 +0500

    add load tests

---
+++ tests_lt/locust.py
@@ -0,0 +1,27 @@
+from locust import HttpLocust, TaskSet
+
+
+def notify(l):
+    json = {
+        'ClientName': 'Кириллица',
+        'ClientAccount': 'qwe',
+        'status': 15,
+        'ID': '123',
+        'Number': 'qwe',
+        'ModifiedBy': 123,
+        'extendedMap': {
+            'qwe': 'qweeqweqw',
+            'qwe2': 'qwe'
+        }
+    }
+    l.client.post('/notify', json=json)
+
+
+class UserBehavior(TaskSet):
+    tasks = {notify: 1}
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
